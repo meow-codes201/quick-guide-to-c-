7374db2f976c8c79202ff46c0972dcf43b623521
code web <humairat291@gmail.com>
2026-06-23 14:58:28 +0530

    Hello World with basic output and comments

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+#include<iostream> //header file for input and output objects like cout 
+int main(){ //main function
+    std::cout << "Hello World" << std::endl;
+    std::cout << "I like Pizza" << std::endl;
+    std::cout << "Its really good" << '\n';
+    //This is a comment,it gets ignored - used for note/explanation of code for other programmers(or yourself)
+    /*
+    this 
+    is 
+    a 
+    multi
+    line
+    comment
+    it gets ignored
+    */
+    return 0;
+}
